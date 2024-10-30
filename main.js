9602ffe3128b8db32019a8af708ab79f9245f671
William Rodgers <william.rodgers@stu.ocsb.ca>
2024-10-30 11:14:30 -0400

    I'm getting slower, be sure to go faster.

--- main.js
+++ main.js
@@ -27,3 +27,44 @@ function round(value, decimals) {
 function randInt(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
 }
+
+function user() {
+
+let age = Number(prompt(`What is your age?`))
+
+if (age >= 60)
+
+    console.log(`You qualify for the senior discount!`)
+
+if (age < 16)
+
+    console.log(`You're not old enough to drive yet.`)
+
+if (age == 44)
+
+    console.log(`You're the same age as Mr.Squirrel`)
+
+let user_name = prompt(`What is your name?`)
+
+if (user_name == "Mr. Squirrel")
+
+    console.log(`🐿️`)
+
+if (user_name.length > 7)
+
+    console.log(`You have a long name.`)
+
+let quiz = Number(prompt(`How long do you think their name is?`))
+
+if (quiz == user_name.length)
+
+    console.log(`That's correct! ✔️`)
+
+if (user_name.length > quiz)
+
+    console.log(`Too low ✖️`)
+
+if (user_name.length > quiz)
+
+    console.log(`Too high ✖️`)
+}
