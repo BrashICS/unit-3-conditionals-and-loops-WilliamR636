d8a3067e25937c73c41ebd2af3907e41aa9c17bf
William Rodgers <william.rodgers@stu.ocsb.ca>
2024-10-31 11:16:37 -0400

    First Day of Spooky Game

--- main.js
+++ main.js
@@ -13,7 +13,7 @@
 
 /*** Event Listeners ***/
 
-
+document.getElementById("Button").addEventListener("click", SpOoOoOoOoky)
 
 /*** Functions ***/
 
@@ -109,4 +109,76 @@ if (value < 0)
 
     return (`There are 0 zeros. 0s? Zer 0's? I don't kn0ws.`)
         
+}
+
+function SpOoOoOoOoky() {
+
+let decision1 = Number(prompt("Are you suuuuuuure you want to continue? It's very spooky."))
+
+if (decision1 == 1){
+
+    let decision2 = Number(prompt("Wow, you're so good at not getting spooked, you already know how to play the game. Tell me, was it spooky?"))
+
+    if (decision2 == 1){
+
+        alert("Yaay! Well if that's the case, I will give you a bonus spook later. BEWARE! MOOHAHAHAHA! Good Luck!")}
+
+    if (decision2 == 2){
+
+        alert("Oh. Well at least it was entertaining enough to come back! Or you're a completionist who wants to see every ending. Either way, since you came back, I will give you an additional spook to make SURE you get scared this time. Good Luck!")}
+
+    let BeginningSecret = 1
+    
+    }
+
+if (decision1 == 2) {
+
+    alert("Well, can't say I don't blame you. It's VERY spooky after all. Once you finish being spooked, you can come back and get EVEN MORE SPOOKED! MUAHAHAHAHA! ...j.k, Happy Halloween!")
+
+    return}
+
+if (decision1 !=1 && decision1 !=2) {
+
+    alert("Well I can't hear you so I assume it's a good spookin' night. Prepare...TO GET SPOOKED! Have Fun and Happy Halloween!")
+
+    let BeginningSecret = 0}
+
+while (Instructions == false) {
+
+let Instructions = false
+
+let decision3 = Number(prompt("To begin, know that to enter decisions, you should type 1 for Yes, and 2 for No. If you type ANYTHING else from beyond this point, you will be booted out of the spook group. Are the instructions clear?"))
+
+if (decision3 == 1) {
+
+    alert("Cool. Let us BEGIN!")
+    Instructions = true}
+
+if (decision3 == 2) {
+
+    alert("No")
+    Instructions = true}
+
+
+if (decision3 !=1 && decision3 !=2) {
+
+    alert("Well, I can't tell so I will repeat my instructions (Btw, there is TOTALLY a secret if you repeat this 43 times. Do it. I know you want to.)")}
+
+    let InstructionsSecret = 0
+    InstructionsSecret ++
+
+    if (InstructionsSecret == 43 && BeginningSecret == 1){
+
+        alert("A faint glow eminates from the (fictional) instructions page. You feel the spookiness growing")}
+
+    if (InstructionsSecret == 43 && BeginningSecret == 0){
+
+        alert("There was no secret. You were lied to. If you really want the secret, restart the game and say Yes to the first question. Then come back here")}
+
+    
+
+}
+
+alert("I will write more later, for now the class is done. Happy Halloween!")
+
 }
